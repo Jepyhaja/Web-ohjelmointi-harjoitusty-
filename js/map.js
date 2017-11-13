2a978c042e25dc1f3c793d7ed5c1f91a948381db
Jussi Ahola <K8270@student.jamk.fi>
2017-11-13 17:26:44 +0200

    user locating added

--- js/map.js
+++ js/map.js
@@ -67,6 +67,40 @@ function distance(lat1, lon1, lat2, lon2) {
           center: {lat: 62.242603, lng: 25.747257},
           zoom: 14
         });
+    
+    infoWindow = new google.maps.InfoWindow;
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Olet tässä.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
+}
+    
+    
+    
+    
         var contentString = "";
     // info window will display above content string
     var infowindow = new google.maps.InfoWindow({
